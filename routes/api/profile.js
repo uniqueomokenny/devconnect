7c71845562088e9b1a77afdf0bbca682cdcf1b13
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-07-14 11:10:08 +0100

    delete experience, education, and profile and user endpoints

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -206,6 +206,60 @@ router.post('/education', passport.authenticate('jwt', {session: false}), (req, 
       profile.save()
         .then(profile => res.json(profile))
     });
-})
+});
+
+
+// @route DELETE api/profile/experience/:exp_id
+// @desc Delete experience from profile
+// @access Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Profile.findOne({user: req.user.id})
+    .then(profile => {
+      // Get removeIndex
+      const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+      // Splice out of array
+      profile.experience.splice(removeIndex, 1);
+
+      // then save profile
+      profile.save().then(profile => res.json(profile));
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+
+// @route DELETE api/profile/education/:edu_id
+// @desc Delete education from profile
+// @access Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Profile.findOne({user: req.user.id})
+    .then(profile => {
+      // Get removeIndex
+      const removeIndex = profile.education.indexOf(req.params.exp_id);
+      // console.log(removeIndex)
+
+      // Splice out of array
+      profile.education.splice(removeIndex, 1);
+
+      // then save profile
+      profile.save().then(profile => res.json(profile));
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      );
+    });
+  }
+);
+
+
 
 module.exports = router;
