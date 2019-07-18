eb42e289629b03103ead5bace4ca78fd23bcbf79
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-07-18 23:26:52 +0100

    getProfileByHandle redux action

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ import EditProfile from './components/edit-profile/EditProfile';
 import AddEducation from './components/add-credentials/AddEducation';
 import AddExperience from './components/add-credentials/AddExperience';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 
 
 // check for token
@@ -58,6 +59,7 @@ class App extends React.Component {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <Route exact path='/profiles' component={Profiles} />
+              <Route exact path='/profile/:handle' component={Profile} />
               <Switch>
                 <PrivateRoute exact path='/dashboard' component={Dashboard} />
               </Switch>
