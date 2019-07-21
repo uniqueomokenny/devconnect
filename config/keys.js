3b986dd36cc038a3868548c89e0b62727ced0f2f
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-07-21 11:47:16 +0100

    production settings

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://127.0.0.1:27017/devconnect",
-  secretOrKey: "specialkey"
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path')
 
 // Routes
 const users = require('./routes/api/users');
@@ -24,8 +25,8 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 // connect to mongoDB
 mongoose.connect(db, {
-    useNewUrlParser: true
-  })
+  useNewUrlParser: true
+})
   .then(() => {
     console.log('mongoDB connectes');
   })
@@ -41,6 +42,17 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  // load any route different from the api routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
